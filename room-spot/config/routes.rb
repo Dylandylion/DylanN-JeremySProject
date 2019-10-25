6dfad1b11146020a107fe6bd0d0b11b466bac854
DylanN <dnguyen099@yahoo.com>
2019-10-25 04:35:13 +0000

    Fixed vertical offset, added some styling to the filter bar

--- room-spot/config/routes.rb
+++ room-spot/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :airbnbs
-    root "airbnbs#index"
-    get "search", to:"search#search"
+  get "home" , to: "search#index"
+  root "search#index"
+  get "search", to:"search#search"
+    
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
